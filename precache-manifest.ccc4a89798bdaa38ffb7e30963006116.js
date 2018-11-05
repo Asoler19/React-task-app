d5271b63f6caa44b57de1d05b00b642842fa1128
Asoler19 <asoler@techlaunch.io>
2018-11-04 19:54:45 -0500

    Updates

---
+++ precache-manifest.ccc4a89798bdaa38ffb7e30963006116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff6b5b6202191190b30a",
+    "url": "/Asoler19/React-task-app/static/css/main.bfe9e566.chunk.css"
+  },
+  {
+    "revision": "ff6b5b6202191190b30a",
+    "url": "/Asoler19/React-task-app/static/js/main.ff6b5b62.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/Asoler19/React-task-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "744b442e162f677cbd43",
+    "url": "/Asoler19/React-task-app/static/js/runtime~main.744b442e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Asoler19/React-task-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "69a4fd6e1bc492864fb3d63d6a7714be",
+    "url": "/Asoler19/React-task-app/index.html"
+  }
+];
